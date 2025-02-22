5b63e0342680595d495dafa11a000e2311328bcb
Mauricio Coronado <mauricio.coronado08@gmail.com>
2025-02-22 14:53:43 -0500

    giftCard-discovery-server: add project structure

---
+++ giftCard-discovery-server/src/main/java/com/mauricioCoronado/giftCard_discovery_server/GiftCardDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.mauricioCoronado.giftCard_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftCardDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ giftCard-discovery-server/src/test/java/com/mauricioCoronado/giftCard_discovery_server/GiftCardDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricioCoronado.giftCard_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftCardDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
