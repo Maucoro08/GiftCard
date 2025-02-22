c44e72ee2bc1344ea1337cc90e7aea0f47f08815
Mauricio Coronado <mauricio.coronado08@gmail.com>
2025-02-22 18:22:35 -0500

    giftCard-auth-service: add project structure

---
+++ giftCard-auth-service/src/main/java/com/mauricioCoronado/giftCard_auth_service/GiftCardAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mauricioCoronado.giftCard_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftCardAuthServiceApplication.class, args);
+	}
+
+}
