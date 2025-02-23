13773b4b87859548ede4475e450bfdc8ab91395e
Mauricio Coronado <mauricio.coronado08@gmail.com>
2025-02-23 14:56:08 -0500

    giftCard-auth-service: feat(infrastructure): add persistence UserSystem

---
+++ giftCard-auth-service/src/main/java/com/mauricioCoronado/giftCard_auth_service/infrastructure/persistence/UserSystemCrudDto.java
@@ -0,0 +1,16 @@
+package com.mauricioCoronado.giftCard_auth_service.infrastructure.persistence;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserSystemCrudDto {
+    private Long id;
+    private String username;
+    private String password;
+}
