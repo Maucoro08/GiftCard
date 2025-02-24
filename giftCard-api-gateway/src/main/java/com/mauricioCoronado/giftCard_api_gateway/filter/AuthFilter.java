a10214a7ee35a014d8732e46982aba1dccec1dae
Mauricio Coronado <mauricio.coronado08@gmail.com>
2025-02-24 11:00:45 -0500

    giftCard-api-gateway-routes: apply token authentication to all routes of giftCard-manager

---
+++ giftCard-api-gateway/src/main/java/com/mauricioCoronado/giftCard_api_gateway/filter/AuthFilter.java
@@ -0,0 +1,60 @@
+package com.mauricioCoronado.giftCard_api_gateway.filter;
+
+import com.mauricioCoronado.giftCard_api_gateway.dto.TokenDto;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+
+    private final WebClient webClient;
+    private static final String AUTH_VALIDATE_URI = "http://localhost:4040/access/auth/jwt";
+    private static final String ACCESS_TOKEN_HEADER_NAME = "accessToken";
+
+    public AuthFilter() {
+        super(Config.class);
+        this.webClient = WebClient.builder().build();
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange);
+            }
+
+            final var tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            final var chunks = tokenHeader.split(" ");
+
+            if (chunks.length != 2 || !chunks[0].equals("Bearer")) {
+                return onError(exchange);
+            }
+
+            final var token = chunks[1];
+
+            return this.webClient
+                    .post()
+                    .uri(AUTH_VALIDATE_URI)
+                    .header(ACCESS_TOKEN_HEADER_NAME, token)
+                    .retrieve()
+                    .bodyToMono(TokenDto.class)
+                    .flatMap(response -> chain.filter(exchange));
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        final var response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.BAD_REQUEST);
+        return response.setComplete();
+    }
+
+    public static class Config {
+        // Configuración opcional si necesitas parámetros adicionales en el filtro.
+    }
+}
